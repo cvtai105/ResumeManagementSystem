70a81c349977a3316c2dde57716a8a9efba82004
tai <cvtai105@gmail.com>
2024-06-29 21:02:50 +0700

    them data access function cho dang nhap

---
+++ backend/DataAccess/DAOs/DoanhNghiepDAO.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess.Data;
+using Microsoft.EntityFrameworkCore;
+using Models.Entities;
+
+namespace DataAccess.DAOs
+{
+    public class DoanhNghiepDAO(AppDbContext context)
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<DoanhNghiep> GetByEmail(string email)
+        {
+            return await _context.DoanhNghieps.FirstAsync(dn => dn.Email == email);
+        }
+
+        public async Task<DoanhNghiep> Add(DoanhNghiep doanhNghiep)
+        {
+            _context.DoanhNghieps.Add(doanhNghiep);
+            await _context.SaveChangesAsync();
+            return doanhNghiep;
+        }
+    }
+}
