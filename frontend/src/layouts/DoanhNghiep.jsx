1b30aaf39b902c206e56adf7239b5135b782e4ca
dora1012 <lephanxuandung1012@gmail.com>
2024-07-01 09:33:23 +0700

    move files

--- frontend/src/layouts/DoanhNghiep.jsx
+++ frontend/src/layouts/DoanhNghiep.jsx
@@ -1,5 +1,5 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar from "./Navbar_DoanhNghiep";
 import Footer from "../components/Footer";
 
 function DoanhNghiepLayout() {
