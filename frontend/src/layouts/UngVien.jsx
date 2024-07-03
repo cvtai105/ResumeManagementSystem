0556bcf406f0dd9a77fca68802d56a36f9023c97
tai <cvtai105@gmail.com>
2024-07-04 00:52:03 +0700

    update header ungvien

--- frontend/src/layouts/UngVien.jsx
+++ frontend/src/layouts/UngVien.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from 'react';
 import { isUngVienAuth } from "../fetchServices/Auth/checkAuth";
 import deleteCookie from "../utils/deleteCookie";
 import { useNavigate  } from "react-router-dom";
+import './nav.css';
 
 
 function UngVienLayout() {
@@ -14,7 +15,6 @@ function UngVienLayout() {
     if(isUngVienAuth()){
       setIsAuth(true);
     }else {
-      nav('/dangnhap')
     }
   }
   , [nav]);
@@ -27,29 +27,43 @@ function UngVienLayout() {
   
   return (
     <>
-      <header className="flex justify-between items-center  bg-royal-blue-transparent py-4 px-8 ">
-          <div className="flex items-center space-x-2">
-              <img src={logo} alt="JobRepo Logo" className="h-14 w-36" />
-          </div>
-          <nav className="flex space-x-12">
-              <NavLink to="" className={({ isActive }) => isActive ? 'text-royal-blue' : 'hover:text-royal-blue'}>Trang Chủ</NavLink>
-              <NavLink to="timviec" className={({ isActive }) => isActive ? 'text-royal-blue' : 'hover:text-royal-blue'}>Tìm việc</NavLink>
-          </nav>
-          { !isAuth &&
-            <div className="flex space-x-8">
-              <NavLink to="dangnhap" className="hover:text-royal-blue">Đăng Nhập</NavLink>
-              <NavLink to="dangky" className="hover:text-royal-blue">Đăng Ký</NavLink>
-            </div>
-          }
+      <nav className="bg-white shadow-md">
+            <div className="max-w-7xlpx-2 sm:px-6 lg:px-8">
+                <div className="relative flex items-center justify-between h-16">
+                    <div className="flex-1 flex  items-center justify-center sm:items-stretch sm:justify-start">
+                        <div className="flex-shrink-0">
+                            <img className="h-12 w-auto" src={logo} alt="Logo" />
+                        </div>
+                        <div className="ml-10 flex ">
+                            <div className="m-auto">
+                                <NavLink to="" className={({ isActive }) => isActive ? "navlink text-dodger-blue" : "navlink"} >Việc làm</NavLink>
+                                <NavLink to="hoso" className={({ isActive }) => isActive ? "navlink text-dodger-blue" : "navlink"} activeClassName="active">Hồ sơ & CV</NavLink>
+                            </div>
+                        </div>
+                    </div>
+                    { !isAuth &&
+                      <div className="">
+                        
+                        <NavLink to="doanhnghiep" className="mx-10" >Đăng tuyển dụng</NavLink>
+                        <NavLink to="dangnhap" className="login" >Đăng nhập</NavLink>
+                        <NavLink to="dangky" className="login" >Đăng ký</NavLink>
+                      </div>
+                    }
 
-          {
-            isAuth &&
-            <div className="flex space-x-8">
-              <button onClick={logoutHandle} className="">Đăng Xuất</button>
+                    {
+                      isAuth &&
+                      <div className="flex">
+                        <div>
+                        <div className="text-sm">Bạn là nhà tuyển dụng?</div>
+                        <NavLink to="doanhnghiep" className="mr-20 text-base font-semibold" >Đăng tuyển ngay</NavLink>
+                        </div>
+                        <NavLink onClick={logoutHandle} className='m-auto'>Đăng xuất</NavLink>
+                      </div>
+                    }
+                    
+                </div>
             </div>
-          }
-          
-      </header>
+        </nav>
       <main>
         <Outlet /> {/* This is where nested routes will be rendered */}
       </main>
