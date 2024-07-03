2a81a9266a236221136f1175210556c56c9f7476
dora1012 <lephanxuandung1012@gmail.com>
2024-07-03 21:27:46 +0700

    fix a little

--- backend/Models/Entities/DotThanhToan.cs
+++ backend/Models/Entities/DotThanhToan.cs
@@ -14,7 +14,7 @@ namespace Models.Entities
         [ForeignKey("NhanVien")]
         public int? NhanVienThanhToanId { get; set; }
         public string? GhiChu { get; set; } = String.Empty;
-        public NhanVien? NhanVienThanhToan { get; set; } = new();
+        public NhanVien? NhanVienThanhToan { get; set; } 
         public int ThanhToanId { get; set; }
         public ThanhToan ThanhToan { get; set; } = new();
         
