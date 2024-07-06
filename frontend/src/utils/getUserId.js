dba7ec65c1e34c6ffcf9bb5727580c0aa3c68d45
tai <cvtai105@gmail.com>
2024-07-07 06:08:33 +0700

    update get id frontend

--- frontend/src/utils/getUserId.js
+++ frontend/src/utils/getUserId.js
@@ -2,8 +2,20 @@
 import getCookie from "./getCookie";
 import jwtDecode from "./jwtDecode";
 
-export default function getUserId() {
-    const token = getCookie('authToken');
+function getUserId(nameCookie) {
+    const token = getCookie(nameCookie);
     const payload = jwtDecode(token);
     return parseInt(payload.nameid);
+}
+
+export function getDoanhNghiepId () {
+    return getUserId('DoanhNghiepAuthToken');  
+}
+
+export function getNhanVienId () {
+    return getUserId('NhanVienAuthToken');  
+}
+
+export function getUngVienId () {
+    return getUserId('UngVienAuthToken');  
 }
