5faef80e80b56c8dcc88a6a302fa331c2f24c0b3
tai <cvtai105@gmail.com>
2024-07-08 06:58:50 +0700

    update connectionstring

--- backend/Application/Program.cs
+++ backend/Application/Program.cs
@@ -59,7 +59,7 @@ builder.Services.AddControllers()
 
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDbConnection"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureConnection"));
 });
 
 builder.Services.AddScoped<NhanVienDAO>();
